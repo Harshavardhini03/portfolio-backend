75a626d88bb04ed5349a9721dbef2491d25e3d9d
harshavardhini03 <harshavardhini@mallow-tech.com>
2026-05-23 19:38:25 +0530

    Simplify migrations configuration in database.php

--- config/database.php
+++ config/database.php
@@ -64,10 +64,7 @@ return [
 
     ],
 
-    'migrations' => [
-        'table'                  => 'migrations',
-        'update_date_on_publish' => true,
-    ],
+    'migrations' => 'migrations',
 
     'redis' => [
         'client'  => env('REDIS_CLIENT', 'phpredis'),
